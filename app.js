fa727490952ced1956e6939a53711d566848d6c8
Sean <Spglancy@gmail.com>
2018-10-11 20:03:42 -0700

    init

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
 const bodyParser = require('body-parser');
 var Post = require('./models/post.js');
@@ -8,23 +9,53 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 app.get('/',(req,res) => {
     res.redirect('/posts')
 })
 
+app.get('/posts/:id/edit', (req, res) => {
+  Post.findById(req.params.id, function(err, post) {
+    res.render('post-edit', {post: post});
+  })
+})
+
 app.post('/posts', (req, res) => {
     Post.create(req.body).then((post) => {
         console.log(post)
-        res.redirect(`/posts/`) // Redirect to reviews/:id
+        res.redirect(`/posts/${post._id}`) // Redirect to post/:id
     }).catch((err) => {
         console.log(err.message)
     })
 })
 
-// app.get('/posts/:id', (req,res) => {
-//     res.render(`/singlepost/${post._id}`)
-// })
+app.put('/posts/:id', (req, res) => {
+  Post.findByIdAndUpdate(req.params.id, req.body)
+    .then(post => {
+      res.redirect(`/posts/${post._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+app.delete('/posts/:id', function(req, res) {
+    console.log("DELETE post")
+    Post.findByIdAndRemove(req.params.id).then((post) => {
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
+})
+
+app.get('/posts/:id', (req, res) => {
+  Post.findById(req.params.id).then((post) => {
+    res.render('singlepost', { post: post })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.get('/posts', (req, res) => {
   Post.find().then(post => {
@@ -35,10 +66,6 @@ app.get('/posts', (req, res) => {
     .catch((err) => {
     console.log(err);
     })
-})
-
-app.get('/new-post', (req,res) => {
-    res.render('new-post', {});
 })
 
 app.listen(3000, () => {

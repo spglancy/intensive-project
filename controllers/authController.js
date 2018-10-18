19a57808d5fcfebc56bd9da16b43fa934c9f6b88
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-10-18 10:12:12 -0700

    formatting the code

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,42 +7,55 @@ const bcrypt = require('bcryptjs');
 const config = require('../config');
 const verifyToken = require('./verifyToken.js');
 
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json());
-
-router.get('/', (req,res) => {
-    res.render('signup');
+/**
+ * Renders Signup template
+ */
+router.get('/', (req, res) => {
+  res.render('signup');
 })
 
-router.get('/login', (req,res) => {
-    res.render('login');
+/**
+ * Renders login page
+ */
+
+router.get('/login', (req, res) => {
+  res.render('login');
 })
 
-router.post('/register', function(req, res) {
+/**
+ *  Register usr with this endpoint 
+ */
+
+router.post('/register', function (req, res) {
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
   User.create({
-    name : req.body.name,
-    email : req.body.email,
-    password : hashedPassword
-},
-  function (err, user) {
-    if (err) return res.status(500).send("There was a problem registering the user.")
-    // create a token
-    var token = jwt.sign({ id: user._id }, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-  });
-    res.status(200).redirect('/posts');
-  });
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  },
+    function (err, user) {
+      if (err) return res.status(500).send("There was a problem registering the user.")
+      // create a token
+      var token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 86400 // expires in 24 hours
+      });
+      res.status(200).redirect('/posts');
+    });
 });
 
-router.post('/login', function(req, res) {
+
+/**
+ * Logs the user in.
+ */
+
+router.post('/login', function (req, res) {
   User.findOne({ email: req.body.email }, function (err, user) {
     if (err) return res.status(500).send('Error on the server.');
     if (!user) return res.status(404).send('No user found.');
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) return res.status(401);
-    });
-    res.redirect(`/posts/${user.userId}`);
   });
+  res.redirect(`/posts/${user.userId}`);
+});
 
 module.exports = router;
--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,21 +3,21 @@ const express = require('express');
 const commentController = express.Router();
 const post = require('../models/post.js');
 
-    commentController.post('/posts/comments/new/',(req,res) => {
-	Comment.create({content: req.body.content, postId: req.body.postId})
-	.then(comment =>{
-		res.redirect(`/posts/${req.body.postId}`);
-		console.log(req.body)
-		});
-	})
+commentController.post('/posts/comments/new/', (req, res) => {
+    Comment.create({ content: req.body.content, postId: req.body.postId })
+        .then(comment => {
+            res.redirect(`/posts/${req.body.postId}`);
+            console.log(req.body)
+        });
+})
 
-   commentController.delete('/posts/comments/:id', function (req, res) {
-       console.log("DELETE comment");
-       Comment.findByIdAndDelete(req.params.id).then((comment) => {
-           res.redirect('/posts/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
+commentController.delete('/posts/comments/:id', function (req, res) {
+    console.log("DELETE comment");
+    Comment.findByIdAndDelete(req.params.id).then((comment) => {
+        res.redirect('/posts/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
 })
 
 module.exports = commentController;

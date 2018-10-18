7fd9dfa69ca647252d11d8e0cdb6aab36727c1b7
Sean <Spglancy@gmail.com>
2018-10-18 10:34:16 -0700

    commented out some stuff

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,18 +28,6 @@ router.get('/login', (req, res) => {
 router.post('/register', function (req, res) {
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
   User.create({
-    name : req.body.name,
-    email : req.body.email,
-    password : hashedPassword
-},
-  function (err, user) {
-    if (err) return res.status(500).send("There was a problem registering the user.")
-    // create a token
-    var token = jwt.sign({ id: user._id, name: user.name }, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-  });
-    res.status(200).redirect('/posts');
-  });
     name: req.body.name,
     email: req.body.email,
     password: hashedPassword
@@ -65,9 +53,6 @@ router.post('/login', function (req, res) {
     if (!user) return res.status(404).send('No user found.');
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) return res.status(401);
-    var token = jwt.sign({ id: user._id, name: user.name }, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-  });
   });
   res.redirect(`/posts/${user.userId}`);
 });
